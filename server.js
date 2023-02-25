6782b006635bb79f34e6fa8af5d31d7c05d973d9
shouhei <shohei.nagamura@gmail.com>
2023-02-26 04:32:47 +0900

    express.staticで静的なファイルをレンダリングできるようにした

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+
+// ルーティングができる
 const userRouter = require("./routes/user");
+
+//HTMLでレンダリングできるようにする publicフォルダの中のファイルを読み込む
+app.use(express.static("public"));
+
 
 //ルートパス
 app.get("/", (req, res) => {
     console.log("ルートパスです");
 
     res.send("<h1>Hello</h1>");
-    // res.sendStatus(200)
 })
 
 // userルーティング
