cb92df4d4f1e2eea467d8ba4d063b18ab33df931
shouhei <shohei.nagamura@gmail.com>
2023-02-26 04:44:22 +0900

    ejsエンジンで動的ファイルを生成

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const PORT = 3001;
 const userRouter = require("./routes/user");
 
 //HTMLでレンダリングできるようにする publicフォルダの中のファイルを読み込む
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+//ejsテンプレートエンジンを読み込ませて動的なファイルを生成
+app.set('view engine', 'ejs');
 
 
 //ルートパス
 app.get("/", (req, res) => {
-    console.log("ルートパスです");
+    // console.log("ルートパスです");
 
-    res.send("<h1>Hello</h1>");
+    // res.send("<h1>Hello</h1>");
+
+    res.render('index', { Text: "動的ファイルを生成" })
 })
 
 // userルーティング
